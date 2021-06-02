c17a8144dc4be6ff77fb2767d25da13f168f7320
Robzabel <robzabel88@gmail.com>
2021-06-02 15:15:26 +0100

    moving onto the main API

--- basic.py
+++ basic.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Api, Resource
+
 
 app = Flask(__name__)
 api = Api(app)
@@ -10,8 +11,13 @@ names = {'john':{'age': 32, 'gender': 'male'},
 class HelloWorld(Resource):
     def get(self, name):
         return names[name]
-
-api.add_resource(HelloWorld, '/<string:name>')
+    
+    def put(self,):
+        name = request.get_json('name')
+        age = request.get_json('age')
+        gender = request.get_json('gender')
+        return  name,  age, gender
+api.add_resource(HelloWorld, '/', '/<string:name>')
 
 if __name__ == "__main__":
     app.run(debug = True)
