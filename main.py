7ccf39a1b3c5d49aa61abb39d6f7a717eb8b25bb
Robzabel <robzabel88@gmail.com>
2021-06-03 16:35:52 +0100

    began working with an SQLite DB

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource, reqparse, abort
+from flask_sqlalchemy import SQLAlchemy
 
 
 #Create the FlasK App
@@ -8,30 +9,62 @@ app = Flask(__name__)
 api = Api(app)                
 #Create the Request Parser object
 film_put_args = reqparse.RequestParser()
+#Create the Database object and initialise it with app
+db = SQLAlchemy(app)
+
+
+#Define the app configs for SQL Alchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///film_database.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+#Create the database Model
+class Film(db.Model):
+    """
+    The database to store the films
+    """
+    __tablename__ = "films"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    director = db.Column(db.String(80), nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"<film: name = {self.title}, director = {self.director}, rating out of 10 = {self.rating}>"
+
+#Configure the Database, comment as necessary
+#db.drop_all()
+#db.create_all()
 
 
 #Define the paramaters to parse requests for, extracted data will be stored in parse_args()
-film_put_args.add_argument("title", type=str, help='You must specify the Title of the film!')
-film_put_args.add_argument("director", type=str, help='You must specify the Director of the film!')
-film_put_args.add_argument("rating", type=int, help='You must specify the Rating of the film, 1 - 10!')
+film_put_args.add_argument("title", type=str, help='You must specify the Title of the film!', required =True)
+film_put_args.add_argument("director", type=str, help='You must specify the Director of the film!', required=True)
+film_put_args.add_argument("rating", type=int, help='You must specify the Rating of the film, 1 - 10!', required = True)
 
 
-films = {}
-
-
+#Create a resource for the API
 class Film(Resource):
     """
-    The Film Resource that the API will refer to 
+    The Film Resource for the API 
     """
 
     def get(self, film_id):
-        return films[film_id]
+       
+        return {"Film ID " + str(film_id) : films[film_id]}, 200#Return the requested information and the HTTP code
 
 
     def put(self, film_id):
+        
         args = film_put_args.parse_args() #the parse_args() method searches the request for the criteria in the add_argument statements above
-        return{film_id : args} #Return the data that has been added to the user
+        films[film_id] = args #stores the put data in the films dict with the film_id as the main key
+        return {"message":"Your film has been added!"}, 201 #Return a success message and an HTTP code
 
+
+    def delete(self, film_id):
+
+        del films[film_id]#remove the film from the db
+        return {"message":"Your film has been deleted!"}, 200#return a success message and an HTTP code
 
 #link the resource with the URL string and the dynamin value
 api.add_resource(Film, "/film/<int:film_id>")
--- test_script.py
+++ test_script.py
@@ -2,5 +2,11 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"             #Global Variable for the base URL of the server
 
-response = requests.put(BASE +"film/2", {"title": "Aliens", "director": "James Cameron", "rating": 8})      #Create an object that holds the returned GET data. Use requests.get(BASE + "path/to/resource") you can chane the HTTP method to GET,POST,DELETE,PUT
+response = requests.put(BASE +"film/2", {"title": "Aliens", "director": "James Cameron", "rating": 8})      #Create an object that holds the returned GET data. Use requests.get(BASE + "path/to/resource") you can change the HTTP method to GET,POST,DELETE,PUT
 print(response.json())                      #Print the response in a JSON format. use the .json() method to return the data not the response object
+input()
+response = requests.get(BASE +"film/2")
+print(response.json())
+input()
+response = requests.delete(BASE +"film/2")
+print(response.json())
