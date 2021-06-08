29fd7a1ad681b5e22df108cf24cf18a60bcad4b4
Robzabel <robzabel88@gmail.com>
2021-06-08 15:14:42 +0100

    completed writing the API

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from flask import Flask
-from flask_restful import Api, Resource, reqparse, abort
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import exc
+
 
 
 #Create the FlasK App
 app = Flask(__name__)
 #Create the API wrapper and pass the app to it    
 api = Api(app)                
-#Create the Request Parser object
-film_put_args = reqparse.RequestParser()
 #Create the Database object and initialise it with app
 db = SQLAlchemy(app)
 
@@ -36,11 +36,27 @@ class Film(db.Model):
 #db.drop_all()
 #db.create_all()
 
-
+#Create the PUT Request Parser object
+film_put_args = reqparse.RequestParser()
 #Define the paramaters to parse requests for, extracted data will be stored in parse_args()
 film_put_args.add_argument("title", type=str, help='You must specify the Title of the film!', required =True)
 film_put_args.add_argument("director", type=str, help='You must specify the Director of the film!', required=True)
 film_put_args.add_argument("rating", type=int, help='You must specify the Rating of the film, 1 - 10!', required = True)
+
+#Create the UPDATE Request Parser object
+film_update_args = reqparse.RequestParser()
+#Define the paramaters to parse requests for, extracted data will be stored in parse_args(). Do not make all required
+film_update_args.add_argument("title", type=str, help='You must specify the Title of the film!')
+film_update_args.add_argument("director", type=str, help='You must specify the Director of the film!')
+film_update_args.add_argument("rating", type=int, help='You must specify the Rating of the film, 1 - 10!')
+
+#Create the Resource field to serialize the db data object into JSON 
+resource_fields = {
+    "id" : fields.Integer,
+    "title" : fields.String,
+    "director" : fields.String,
+    "rating" : fields.Integer
+}
 
 
 #Create a resource for the API
@@ -48,26 +64,71 @@ class Film_db(Resource):
     """
     The Film Resource for the API 
     """
-
+    #use the decorator to serialize the returned data
+    @marshal_with(resource_fields)
     def get(self, film_id):
-        output = Film.query.get(id = film_id)
-        return output
-
-
+        #Query the DB for the film date depending on the film_id provided
+        output = Film.query.filter_by(id=film_id).first()
+        if output:
+            return output, 200
+        else: 
+            abort(404, message ="There is no film with the ID " + str(film_id))
+      
+    
     def put(self, film_id):
-        
-        args = film_put_args.parse_args() #the parse_args() method searches the request for the criteria in the add_argument statements above
-        films[film_id] = args #stores the put data in the films dict with the film_id as the main key
-        return {"message":"Your film has been added!"}, 201 #Return a success message and an HTTP code
+        #Try and catch the put attempt
+        try:
+            #the parse_args() method searches the request for the criteria in the add_argument statements above
+            args = film_put_args.parse_args() 
+            #Add the args to the model object then commit the changes
+            put_film = Film(id=film_id, title=args['title'], director=args['director'], rating=args['rating'])
+            db.session.add(put_film)
+            db.session.commit()
+            return {"message":"Your film has been added!"}, 201 #Return a success message and an HTTP code
+        #IF the fim_id is not found send an error message
+        except exc.IntegrityError:
+            abort(409, message ="A film with the ID " + str(film_id) + " already exists in the database. Please use a different ID")
+
+
+    #use the decorator to serialize the returned data        
+    @marshal_with(resource_fields)
+    def patch(self, film_id):
+        #Create the argument object
+        args = film_update_args.parse_args()
+        #create the Model object
+        output = Film.query.filter_by(id=film_id).first()
+        #Abort if there is nothing to update
+        if not output:
+            abort(404, message ="A film with the ID " + str(film_id) + " does not exist in the database. Please use a different ID")
+        #chek if the args attributes are not None
+        if args["title"]:
+            output.title = args["title"]
+        if args["director"]:
+            output.director = args["director"]
+        if args["rating"]:
+            output.rating = args["rating"]
+        #commit the changes to the Database
+        db.session.commit()
+        #return the info to the user
+        return output, 200
 
 
     def delete(self, film_id):
+        #find the film to be deleted
+        output = Film.query.filter_by(id=film_id).first()
+        if output:
+            #delete the film and commit the changes
+            db.session.delete(output)
+            db.session.commit()
+            #return a success message and an HTTP code
+            return {"message":"Your film has been deleted!"}, 200
+        else:
+            #IF the fim_id is not found send an error message 
+            abort(404, message ="A film with the ID " + str(film_id) + " does not exist in the database. Please use a different ID")
 
-        del films[film_id]#remove the film from the db
-        return {"message":"Your film has been deleted!"}, 200#return a success message and an HTTP code
 
-#link the resource with the URL string and the dynamin value
-api.add_resource(Film, "/film/<int:film_id>")
+#link the resource with the URL string and the dynamic value
+api.add_resource(Film_db, "/film/<int:film_id>")
 
 
 #If this file is executed, not imported, run the application
